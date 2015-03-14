37270aee92fa58dd8ee843a30c629366b7081596
Lau <laruku.cosmes@gmail.com>
2015-03-14 18:10:03 +0000

    first commit

---
+++ practice_3/templates/links/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+	@include('links._item', compact('link'))
+@stop
---
+++ project/templates/index.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+ 
+<html lang="en">
+     <head>
+         <title> Web App </title>
+         <!-- Bootstrap CSS -->
+         <link href= "/css/bootstrap/bootstrap.css" rel="stylesheet">
+
+         <link href="/css/style.css" rel="stylesheet">
+         <!-- font-awesom-->
+         <link href= "/css/font-awesome/font-awesome.css" rel="stylesheet">
+ 
+     </head>
+     <body>
+        <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+             <div class="container-fluid">
+                    <div class="navbar-header">
+                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-header">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                         </button>
+                       <a class="navbar-brand" href="#">Brand</a>
+                    </div>
+               </div>
+          </nav>
+ 
+        <div class="container">
+              <div class="row">   
+                  <div class="jumbotron" id ="boton1">
+                      <h1 class="text-center"> Hello~~ \(OoO)/</h1>
+                  </div>
+              </div>
+        </div>
+        <div class="container">
+             <div class="row">
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<form action="/" method="POST" role="form">
+						<legend>Add new</legend>
+						<div class="form-group">
+							<label for="">New ToDo Item</label>
+							<input type="text" class="form-control" id="" placeholder="Input field">
+						</div>
+						<button type="submit" class="btn btn-primary pull-right">Submit</button>
+					</form>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<ul class="list-group">
+						<li class="list-group-item active">
+                            <button type="button" class="btn btn-default">
+								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+							</button>
+							 To Do Item 1
+							<div class="btn-group-md pull-right" role="group" aria-label="...">
+								
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+						<li class="list-group-item">
+						    <button type="button" class="btn btn-default">
+								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+							</button>
+							To Do Item 2
+							<div class="btn-group-md pull-right" role="group" aria-label="...">
+								
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+						<li class="list-group-item">
+						    <button type="button" class="btn btn-default">
+								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+							</button>
+							To Do Item 3
+							<div class="btn-group-md pull-right" role="group" aria-label="...">
+								
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+						<li class="list-group-item">
+						    <button type="button" class="btn btn-default">
+								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+							</button>
+							To Do Item 4
+							<div class="btn-group-md pull-right" role="group" aria-label="...">
+								
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+						<li class="list-group-item">
+						    <button type="button" class="btn btn-default">
+								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+							</button>
+							To Do Item 5
+							<div class="btn-group-md pull-right" role="group" aria-label="...">
+								
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+						<li class="list-group-item">
+						    <button type="button" class="btn btn-default">
+								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+							</button>
+							To Do Item 6
+							<div class="btn-group-md pull-right" role="group" aria-label="...">
+								
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+								</button>
+								<button type="button" class="btn btn-default">
+									<span class="glyphicon glyphicon-save" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+					</ul>
+				</div>
+		    	<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<a class="btn btn-block btn-social btn-google-plus" href="https://plus.google.com/u/0/105601463048696162263/posts" target="_blank">
+						<i class="fa fa-google-plus"></i>
+                        Google++
+					</a>
+					<a class="btn btn-block btn-social btn-twitter" href="https://twitter.com/laruku_cosmes" target="_blank">
+						<i class="fa fa-twitter"></i>
+						Twitter
+					</a>
+					<a class="btn btn-block btn-social btn-facebook" href="https://www.facebook.com/laruku.cosmes" target="_blank">
+						<i class="fa fa-facebook"></i>
+						Facebook
+					</a>
+					<a class="btn btn-block btn-social btn-github" href="https://github.com/LauCosmes/practice_1" target="_blank">
+						<i class="fa fa-github"></i>
+						GitHub
+					</a>
+				</div>	
+		     </div>
+		</div>
+		<hr>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<p>Copyright &copy; IESO Basic Practice</p>
+					</div>
+				</div>
+			</div>
+		</footer> 
+     </body>
+</html>
